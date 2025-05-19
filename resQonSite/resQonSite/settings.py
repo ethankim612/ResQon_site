9b0ee0a25c229db11fd806357269f8dcbecfda5a
ethankim612 <ethansw612@gmail.com>
2025-05-19 22:39:02 +0900

    Update STATIC_URL and add MEDIA_URL configuration in settings.py

--- resQonSite/resQonSite/settings.py
+++ resQonSite/resQonSite/settings.py
@@ -120,13 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Static files configuration
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEBUG = True
 
